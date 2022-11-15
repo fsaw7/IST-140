8b1f709a91d7e501888aa7ea6a8a7dccb9c0b304
fsaw7 <116685102+fsaw7@users.noreply.github.com>
2022-11-15 14:41:07 -0500

    Add files via upload

---
+++ HelloMarsApp/HelloMarsApp/src/hellomarsapp/HelloMarsApp.java
@@ -0,0 +1,26 @@
+/*
+ * Fatou Sawaneh
+ * HelloMarsApp
+ * fjs5286@psu.edu
+ * 9/21/21
+ *
+ */
+package hellomarsapp;
+
+/**
+ * HelloMarsApp.java
+ * @author fatousawaneh
+ * This prints out the statement, "Greetings Martians!"
+ */
+public class HelloMarsApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.print("Greetings ");
+       System.out.print("Martians ");
+       System.out.println("!");
+    }
+    
+}
