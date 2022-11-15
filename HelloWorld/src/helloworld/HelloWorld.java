f9a21ec694dcecb5fa2ea993d79381c59c999fae
fsaw7 <116685102+fsaw7@users.noreply.github.com>
2022-11-15 14:39:38 -0500

    Add files via upload

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * Fatou Sawaneh
+ * Lab #1
+ * 9/14/21
+ *fjs5286@psu.edu
+ */
+package helloworld;
+
+/**
+ * HelloWorld.java
+ * @author fatousawaneh
+ * This program prints "Hello World!" to the command line. 
+ */
+public class HelloWorld {
+
+    //The main method is important because every Java application starts running at the main method.
+    public static void main(String[] args) {
+       System.out.println("Hello World!");
+    }
+    
+}
+
